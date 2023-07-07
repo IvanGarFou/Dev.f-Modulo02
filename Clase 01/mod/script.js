348c9948b1bb69bcbd354fed125fd983d04b13d4
Roberto Romero <roberto@arrenda.mx>
2023-07-06 21:23:46 -0600

    mod %

---
+++ Clase 01/mod/script.js
@@ -0,0 +1,12 @@
+let n = 10;
+
+/*
+3 / 2 = 1.5 //Adultos
+3 / 2 = 1 y sobra 1 // Niños
+10 / 4 = 2 y sobra 2
+*/
+console.log(n/4); // 2.5
+console.log(n % 4) //Es 2
+
+// n % 2 == 0  es par
+// n % 2 == 1 es impar
