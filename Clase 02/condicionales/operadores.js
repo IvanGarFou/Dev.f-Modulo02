353679fd2410a40c77d4bcbcc3935dfc7395450e
Roberto Romero <roberto@arrenda.mx>
2023-07-06 20:20:03 -0600

    Operadores Logicos

---
+++ Clase 02/condicionales/operadores.js
@@ -0,0 +1,21 @@
+/* Operadores Relacionales */
+
+let x = 1.5; // numero
+let y = "1.5"; //cadena
+
+console.log(x==y) // true
+console.log(x===y) // false
+
+let cadena1= "Pikachu";
+let cadena2= "Pikachu";
+let cadena3= "pikachu"
+
+console.log(cadena1 === cadena2) // true
+console.log(cadena1 === cadena3) // false
+console.log(cadena2 !== cadena3) // true
+
+/* Operadores Lógicos */
+console.log((10>20) && (10<5))
+// console.log((10>20) && (10<5) && (10!=10)) 
+
+
---
+++ Clase 02/repaso-operadores/script.js
@@ -0,0 +1,14 @@
+var a = prompt("Ingresa un numero");
+var b = parseInt(prompt("Ingresa otro numero"));
+var nombre = prompt("Como te llamas?");
+
+var a2 = parseInt(a);
+var c = b*a2;
+var d = b/a2;
+var e = b*b*a2+1
+
+console.log(nombre);
+
+alert(nombre);
+
+document.write("Mi nombre es "+ nombre)
